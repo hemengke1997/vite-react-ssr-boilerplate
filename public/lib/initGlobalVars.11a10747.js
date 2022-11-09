67bb97b5b9848561be3057304ec2283bf1808eec
hemengke1997 <49073282+hemengke1997@users.noreply.github.com>
2022-11-09 18:27:55 +0800

    chore: update

---
+++ public/lib/initGlobalVars.11a10747.js
@@ -0,0 +1,10 @@
+'use strict'
+;(() => {
+  ;(function () {
+    function t(i) {
+      return document.documentElement.getAttribute(i)
+    }
+    let e = String(!0)
+    window.isMobile = t('is-mobile') === e
+  })()
+})()
