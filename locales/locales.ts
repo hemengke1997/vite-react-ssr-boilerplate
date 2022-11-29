48137e9cf678de1a425d193b38ecb43ac0193c0d
hemengke1997 <49073282+hemengke1997@users.noreply.github.com>
2022-11-29 21:56:03 +0800

    perf: localesMap

--- locales/locales.ts
+++ locales/locales.ts
@@ -1,4 +1,10 @@
-export const localesMap = {
-  en: 'en',
-  zh: 'zh',
-}
+const resourcesOrigin = import.meta.glob('./*/index.ts', {})
+
+const localesMap: Record<string, string> = {}
+
+Object.keys(resourcesOrigin).forEach((k) => {
+  const dir = /\.\/(.+)\//.exec(k)![1]
+  localesMap[dir] = dir
+})
+
+export { localesMap }
