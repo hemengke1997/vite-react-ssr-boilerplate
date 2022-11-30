830623836b7ed774dc09bbf6da79056fea26e731
hemengke1997 <49073282+hemengke1997@users.noreply.github.com>
2022-11-30 21:24:50 +0800

    test: tsc

--- renderer/pageType.d.ts
+++ renderer/pageType.d.ts
@@ -1,45 +1,41 @@
-import { i18n } from 'i18next'
+declare namespace PageType {
+  type PageProps = Partial<{
+    title: string
+    description: string
+    keywords: string
+    isMobile: boolean
+    /**
+     * @description 是否检查平台（移动端/pc）
+     */
+    checkPlatform: boolean
+    /**
+     * 是否强制开启 vconsole
+     */
+    vconsole?: boolean
+    /**
+     * data from backend
+     */
+    data?: Record<string, any>
+  }>
 
-declare global {
-  declare namespace PageType {
-    type PageProps = Partial<{
-      title: string
-      description: string
-      keywords: string
-      isMobile: boolean
-      /**
-       * @description 是否检查平台（移动端/pc）
-       */
-      checkPlatform: boolean
-      /**
-       * 是否强制开启 vconsole
-       */
-      vconsole?: boolean
-      /**
-       * data from backend
-       */
-      data?: Record<string, any>
-    }>
+  interface PageContext {
+    Page: React.FC<PageProps>
+    pageProps: PageProps
+    locale: string
+    redirectTo?: string
+  }
 
-    interface PageContext {
-      Page: React.FC<PageProps>
+  type onBeforeRender = Promise<{
+    pageContext: {
       pageProps: PageProps
+    }
+  }>
+
+  type onBeforeRoute = Promise<{
+    pageContext: {
+      urlOriginal: string
       locale: string
       redirectTo?: string
     }
-
-    type onBeforeRender = Promise<{
-      pageContext: {
-        pageProps: PageProps
-      }
-    }>
-
-    type onBeforeRoute = Promise<{
-      pageContext: {
-        urlOriginal: string
-        locale: string
-        redirectTo?: string
-      }
-    }>
-  }
+  }>
 }
