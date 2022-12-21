bbca1b1b9335a6774e69d7139f034ac66c2d12c4
hemengke1997 <49073282+hemengke1997@users.noreply.github.com>
2022-12-21 18:29:07 +0800

    chore: update [ci skip]

--- config/vite/utils/helper.ts
+++ config/vite/utils/helper.ts
@@ -1,25 +1,7 @@
 import { createHash } from 'node:crypto'
-import { isObject } from 'lodash-es'
-import type { PreRenderedChunk } from 'rollup'
-
-export function deepMerge<T = any>(src: T, target: T): T {
-  let key: string
-  for (key in target) {
-    src[key] = isObject(src[key]) ? deepMerge(src[key], target[key]) : (src[key] = target[key])
-  }
-  return src
-}
 
 export function getContentHash(chunk: string | Uint8Array) {
   return createHash('sha256').update(chunk).digest('hex').substring(0, 8)
-}
-
-export function getHash(chunkInfo: PreRenderedChunk) {
-  return getContentHash(
-    Object.values(chunkInfo.modules)
-      .map((m) => m.code)
-      .join(),
-  )
 }
 
 export function injectEnv(envConf: Record<string, any>): ImportMetaEnv {
--- src/service/utils.ts
+++ src/service/utils.ts
@@ -1,13 +1,21 @@
-import { isObject } from 'lodash-es'
+import { cloneDeep } from 'lodash-es'
+
+function is(val: unknown, type: string) {
+  return toString.call(val) === `[object ${type}]`
+}
+
+function isObject(val: any): val is Record<any, any> {
+  return val !== null && is(val, 'Object')
+}
 
 export function deepMerge<T = any>(src: any = {}, target: any = {}): T {
   let key: string
+  const res: any = cloneDeep(src)
   for (key in target) {
-    src[key] = isObject(src[key]) ? deepMerge(src[key], target[key]) : (src[key] = target[key])
+    res[key] = isObject(res[key]) ? deepMerge(res[key], target[key]) : (res[key] = target[key])
   }
-  return src
+  return res
 }
-
 export function joinTimestamp(join: string, restful = false): string | object {
   if (!join) {
     return restful ? '' : {}
